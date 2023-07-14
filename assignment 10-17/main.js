56ddc1300b3cc211397ee2804e0376a9125b3e6e
m-elshafei <moustafaelshafhy@gmail.com>
2023-07-15 02:20:23 +0300

    assignment_from_10_to_17

---
+++ assignment 10-17/main.js
@@ -0,0 +1,25 @@
+let NumberOne = 10,
+  NumberTwo = 20;
+console.log(String(NumberOne) + NumberTwo);
+console.log(`${NumberOne}${NumberTwo}`);
+console.log(`${typeof NumberOne}${typeof NumberTwo}`);
+console.log(NumberOne + "\n" + NumberTwo);
+console.log(`${NumberOne}
+${NumberTwo}`);
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+console.log(
+  '`I\'m In\n\\\\\nLove \\\\ """ \'\'\'\n++ With ++\n\\"""\\"""\n""JavaScript""``'
+);
+let a = 21;
+let b = 20;
+console.log("_" + a + "_" + b + a + "_" + b + a + "_" + b + a + "_" + b + "_"); // _21_2021_2021_2021_20_
+let theTitle = "Elzero",
+  theDescription = "Elzero Web School",
+  theDate = "25/10";
+console.log(theTitle + "\n" + theDescription + "\n" + theDate);
+let x = `<h3>Hello ${theTitle}</h3>
+<p>${theDescription}</p>
+<span>${theDate}</span>`;
+let c = x.repeat(4);
+document.write(c)
