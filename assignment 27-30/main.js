d21f0e66cf6c52fa301f4e294a85309a1b2c23c3
m-elshafei <moustafaelshafhy@gmail.com>
2023-08-09 15:37:38 +0300

    assignment_from_27_to_30

---
+++ assignment 27-30/main.js
@@ -0,0 +1,6 @@
+let a = "HHHHHHHHHHHHHHHHHHHH";
+console.log(a.charAt(2).toUpperCase() + a.slice(3,6))
+console.log(a.charAt(13).toUpperCase().repeat(8))
+console.log(a.split(" ",1))
+console.log(`${a.substr(0,6)} ${a.substr(11)}`)
+console.log(a.toUpperCase().toLowerCase(0))
