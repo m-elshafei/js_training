9514a3b7084267726dae7c089ce06b0b33d1800d
m-elshafei <moustafaelshafhy@gmail.com>
2023-10-02 07:46:35 +0300

    assignment_from_38_to_39

---
+++ assignment vedio 37/main.js
@@ -0,0 +1,47 @@
+let a = 10;
+
+if (a < 10) {
+  console.log(10);
+} else if (a >= 10 && a <= 40) {
+  console.log("10 To 40");
+} else if (a > 40) {
+  console.log("> 40");
+} else {
+  console.log("Unknown");
+}
+
+a < 10
+  ? console.log(10)
+  : a >= 10 && a <= 40
+  ? console.log("10 To 40")
+  : a > 40
+  ? console.log("> 40")
+  : console.log("Unknown");
+
+  let st = "Elzero Web School";
+
+if (st.length * 2 == "34") {
+  console.log("Good");
+}
+document.write(st.includes("W"));
+document.write(st.indexOf("W") == "W");
+document.write(st.substring("W"));
+document.write(st.includes("W"));
+if (st.includes("W") === "W") {
+  console.log("Gooood");
+}
+
+// switch (a) {
+//   case (a < 10):
+//     console.log(10);
+//     break;
+//   case (a >= 10 && a <= 40):
+//     console.log("10 To 40");
+//     break;
+//   case (a > 40):
+//     console.log("> 40");
+//     break;
+//   default:
+//     console.log("Unknown");
+// }
+qwwe!\|ّّّّّذذّّّذ
